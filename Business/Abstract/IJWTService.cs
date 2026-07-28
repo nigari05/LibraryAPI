8d2ded03ea36abad89f489f0b5f521f399ceb2d8
Nigari Zulfuqarova <nigarizulfuqarova@gmail.com>
2026-07-28 17:39:57 +0400

    feat: implement JWT token generation

---
+++ Business/Abstract/IJWTService.cs
@@ -0,0 +1,12 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IJWTService
+    {
+        string GenerateToken(User user);
+    }
+}
