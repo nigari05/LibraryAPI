e15dbe11dcb508e6a63fe34cc82f9908d331ef77
Nigari Zulfuqarova <nigarizulfuqarova@gmail.com>
2026-08-04 18:06:06 +0400

    feat(business): add FluentValidation rules for Category DTOs

---
+++ Business/Validation/CategoryValidators/CreateCategoryValidator.cs
@@ -0,0 +1,19 @@
+﻿using Entities.DTOs.CategoryDTOs;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Validation.CategoryValidators
+{
+    public class CreateCategoryValidator : AbstractValidator<CreateCategoryDTO>
+    {
+        public CreateCategoryValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Category name is required.")
+                .MinimumLength(2).WithMessage("Category name must be at least 2 characters long.")
+                .MaximumLength(100).WithMessage("Category name must be at most 100 characters long.");
+        }
+    }
+}
