8abbd9ccd1fc0071b8c5403b14355f1b0d5bb216
Nigari Zulfuqarova <nigarizulfuqarova@gmail.com>
2026-08-04 17:22:14 +0400

    feat(dataaccess): add ICategoryDAL and EfCategoryDAL

---
+++ DataAccess/Absract/ICategoryDAL.cs
@@ -0,0 +1,14 @@
+﻿using Core.DataAccess;
+using Core.Utilities.Pagination;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Absract
+{
+    public interface ICategoryDAL : IRepositorybase<Category>
+    {
+        Task<List<Category>> GetAllAsync(PaginationParameters pagination);
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfCategoryDAL.cs
@@ -0,0 +1,39 @@
+﻿using Core.DataAccess.EntityFramework;
+using Core.Utilities.Pagination;
+using DataAccess.Absract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCategoryDAL : EfRepositorybase<Category, AppDbContext>, ICategoryDAL
+    {
+        private readonly AppDbContext _context;
+
+        public EfCategoryDAL(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Category>> GetAllAsync(PaginationParameters pagination)
+        {
+            IQueryable<Category> query = _context.Categories.Include(x => x.Books);
+
+            if (!string.IsNullOrWhiteSpace(pagination.SortBy) &&
+                pagination.SortBy.Equals("name", StringComparison.OrdinalIgnoreCase))
+            {
+                query = pagination.IsDescending
+                    ? query.OrderByDescending(x => x.Name)
+                    : query.OrderBy(x => x.Name);
+            }
+
+            return await query
+                .Skip((pagination.PageNumber - 1) * pagination.PageSize)
+                .Take(pagination.PageSize)
+                .ToListAsync();
+        }
+    }
+}
